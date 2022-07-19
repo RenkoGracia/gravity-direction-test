cacf4dd3a2fd9c2877aa6fd3ffc8916ce66e2ba4
Arg <60346365+RenkoGracia@users.noreply.github.com>
2022-07-19 23:45:25 +0900

    Add files via upload

--- sys.py
+++ sys.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import random
 import math
 win = visual.Window(size=(800, 800),fullscr=False, screen=1,color='black')
-d0=-90 #gravity degree
+d0=0 #gravity degree
 d1=0 #calibration degree
 d2=d0-180 #start compensation degree
 r0=math.radians(d0)
@@ -16,29 +16,31 @@ transition=(-0.5*math.cos(r0),+0.5*math.sin(r0)) #start transition offset
 for i in range (10):
  vx=3#random.randint(0,300)/100
  vy=3#random.randint(0,300)/100 #randomrize initial velocity
- polygon = visual.ShapeStim(win=win,size=0.03,vertices='circle',lineColor='red',fillColor='red')
+ polygon = visual.ShapeStim(win=win,size=0.03,vertices='circle',lineColor=(0,255,255),fillColor=(0,255,255))
  #stimuli defination
+ border = visual.ShapeStim(win=win,size=2,vertices='circle',lineColor=(255,0,255))
  polygon1 = visual.Line(win=win,
  start=(vx*vy/10*math.cos(r0)-(vy*vy/10-5*(vy/10)**2)*math.sin(r0)+0.8*math.cos(r2),
      vx*vy/10*math.sin(r0)+(vy*vy/10-5*(vy/10)**2)*math.cos(r0)+0.8*math.sin(r2)), 
  end=(vx*vy/10*math.cos(r0)+0.5*math.sin(r1)-(vy*vy/10-5*(vy/10)**2)*math.sin(r0)+0.8*math.cos(r2),
      vx*vy/10*math.sin(r0)+(vy*vy/10-5*(vy/10)**2)*math.cos(r0)-0.5*math.cos(r1)+0.8*math.sin(r2)),
- lineColor='yellow', fillColor='yellow')
+ lineColor=(0,255,255))
  #calibration defination
  start= visual.ShapeStim(win=win,size=0.03,vertices='circle',
- pos=[0.8*math.cos(r2),+0.8*math.sin(r2)],lineColor='red',fillColor='red')
+ pos=[0.8*math.cos(r2),+0.8*math.sin(r2)],lineColor=(0,255,255),fillColor=(0,255,255))
  end= visual.ShapeStim(win=win,size=0.03,vertices='circle',
  pos=[vx*limit*math.cos(r0)-(vy*limit-5*limit**2)*math.sin(r0)+0.8*math.cos(r2),
      vx*limit*math.sin(r0)+(vy*limit-5*limit**2)*math.cos(r0)+0.8*math.sin(r2)],
-     lineColor='blue',fillColor='blue')
+     lineColor=(255,0,255),fillColor=(255,0,255))
  mark = visual.ShapeStim(win=win,size=0.03,vertices='circle',
  pos=[vx*vy/10*math.cos(r0)-(vy*vy/10-5*(vy/10)**2)*math.sin(r0)+0.8*math.cos(r2),
      vx*vy/10*math.sin(r0)+(vy*vy/10-5*(vy/10)**2)*math.cos(r0)+0.8*math.sin(r2)],
-     lineColor='white',fillColor='white')
+     lineColor=(255,0,255),fillColor=(255,0,255))
  #three marks definations
  t=0
  #trial loop
  while True:
+    border.draw()
     if event.getKeys('escape'):
      win.close()
     start.draw()
@@ -56,8 +58,8 @@ for i in range (10):
      print(d1)
      break
     if vy-10*t<=0:
-     polygon.fillColor='blue'
-     polygon.lineColor='blue'
+     polygon.fillColor=(255,0,255)
+     polygon.lineColor=(255,0,255)
      mark.draw()
     if t<limit:
      t =0.001+t
